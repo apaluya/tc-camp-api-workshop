e2c019743ad1789e57e71595503bb06c0f14b5fc
Paul Wallace <pwallac@synopsys.com>
2019-04-24 20:31:34 -0700

    revising build script, updating travis, adding a license

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-pandoc -t revealjs -s -o $1.html $1 -V revealjs-url=https://revealjs.com -V transition=cube -V theme=sky -i
+# The script takes a filename as a parameter.
+# Remove the extension from the filename
+filename="$1"
+name="${filename%.*}"
+
+# Using pandoc to build a standalone revealjs slide presentation
+pandoc -t revealjs -s -o $name.html $name.md -V revealjs-url=https://revealjs.com -V transition=cube -V theme=sky -i
